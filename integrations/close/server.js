e3e715f4150859bd65a5ebced59610b7aa3a2ef7
Zach Sents <zachsents@gmail.com>
2024-02-01 19:29:10 +0000

    Tried adding slash to token URL cuz maybe that makes a difference ???

--- integrations/close/server.js
+++ integrations/close/server.js
@@ -1,7 +1,7 @@
 export default {
     oauth2: {
         authUrl: "https://app.close.com/oauth2/authorize",
-        tokenUrl: "https://api.close.com/oauth2/token",
+        tokenUrl: "https://api.close.com/oauth2/token/",
         scopes: ["offline_access"],
         profileUrl: "https://api.close.com/api/v1/me",
         getDisplayName: (profile) => profile.email,

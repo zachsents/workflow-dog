79acb8e7d1b566443a0ce283d2ac393f541c8cab
Zach Sents <zachsents@gmail.com>
2024-02-01 01:05:05 +0000

    Fixed callback for integratipn server

--- integration-server/callback.ts
+++ integration-server/callback.ts
@@ -2,6 +2,6 @@
 
 export function callbackUrl(serviceName: string) {
     return process.env.NODE_ENV === "production"
-        ? `https://api.workflow.dog/integration/${serviceName}/callback`
-        : `http://localhost:${process.env.PORT}/integration/${serviceName}/callback`
+        ? `https://integrate-e45frdiv4a-uc.a.run.app/service/${serviceName}/callback`
+        : `http://localhost:${process.env.PORT}/service/${serviceName}/callback`
 }

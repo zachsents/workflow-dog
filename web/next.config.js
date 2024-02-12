dad5ffd73e352ef428a026da2005903beecad9ff
Zach Sents <zachsents@gmail.com>
2024-02-12 23:45:07 +0000

    Added redirect to /workflows

--- web/next.config.js
+++ web/next.config.js
@@ -18,4 +18,14 @@ module.exports = {
 
         return config
     },
+
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/workflows",
+                permanent: true,
+            },
+        ]
+    },
 }

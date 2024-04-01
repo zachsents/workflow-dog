c55d13e531ca4965da9f1f8374989773e5641b64
Zach Sents <zachsents@gmail.com>
2024-04-01 16:59:25 -0600

    Changed compose/decompose nodes to use lodash

--- packages/basic/nodes/compose-object/execution.ts
+++ packages/basic/nodes/compose-object/execution.ts
@@ -1,11 +1,16 @@
 import type { ExecutionNodeDefinition } from "@types"
 import type shared from "./shared.js"
+import _ from "lodash"
 
 
 export default {
     action: ({ properties }) => {
         return {
-            object: properties
+            object: Object.entries(properties).reduce((acc, [key, value]) => {
+                if (value !== undefined)
+                    _.set(acc, key, value)
+                return acc
+            }, {})
         }
     },
 } satisfies ExecutionNodeDefinition<typeof shared>
--- packages/basic/nodes/decompose-object/execution.ts
+++ packages/basic/nodes/decompose-object/execution.ts
@@ -1,5 +1,6 @@
 import type { ExecutionNodeDefinition } from "@types"
 import type shared from "./shared.js"
+import _ from "lodash"
 
 
 export default {
@@ -7,9 +8,12 @@ export default {
         if (!object)
             throw new Error("Didn't receive an object")
 
-        const keys = Array.from(new Set(node.data.outputs.map(output => output.name!)))
+        const keys = Array.from(
+            new Set(node.data.outputs.map(output => output.name!))
+        )
+
         return {
-            properties: Object.fromEntries(keys.map(key => [key, object[key]]))
+            properties: Object.fromEntries(keys.map(key => [key, _.get(object, key)]))
         }
     },
 } satisfies ExecutionNodeDefinition<typeof shared>

5822701f47456c45095b65185d6eb5c9458d64ed
Zach Sents <zachsents@gmail.com>
2024-04-10 20:33:24 -0600

    repo cleanup come on

--- shared/application.js
+++
@@ -1,8 +0,0 @@
-
-export const BASE_URL = process.env.NODE_ENV === "production" ?
-    "https://app.workflow.dog" :
-    "http://localhost:3000"
-
-export const FUNCTIONS_URL = process.env.NODE_ENV === "production" ?
-    functionName => `https://us-central1-workflow-dog.cloudfunctions.net/${functionName}` :
-    functionName => `http://localhost:5001/workflow-dog/us-central1/${functionName}`

a5235cdabbac8907d59cc869ada57331a7cd2a33
Zach Sents <zachsents@gmail.com>
2024-04-20 03:14:11 -0600

    added logging

--- web/app/api/internal/gmail-inbox-watch-subscriber/route.ts
+++ web/app/api/internal/gmail-inbox-watch-subscriber/route.ts
@@ -1,11 +1,15 @@
 import { errorResponse } from "@web/lib/server/router"
 import { getServiceAccountToken } from "@web/lib/server/service-accounts"
 import { supabaseServerAdmin } from "@web/lib/server/supabase"
+import { google } from "googleapis"
 import { NextRequest, NextResponse } from "next/server"
 import { z } from "zod"
 
 
 export async function POST(req: NextRequest) {
+
+    console.log(await req.json())
+
     const bodyValidation = bodySchema.safeParse(await req.json())
     if (!bodyValidation.success)
         return errorResponse("Invalid request body", 400)
@@ -32,19 +36,43 @@ export async function POST(req: NextRequest) {
 
     const token = await getServiceAccountToken(supabase, serviceAccountId) as { access_token: string } | null
 
-    const workflowIds = await supabase
+    const workflows = await supabase
         .from("workflows")
-        .select("id")
+        .select("id, startHistoryId:trigger->data->>historyId, trigger")
         .eq("trigger->>type", "https://triggers.workflow.dog/google/gmail-email-received")
         .eq("trigger->config->>googleAccount", serviceAccountId)
         .throwOnError()
-        .then(q => q.data?.map(w => w.id) || [])
+        .then(q => q.data || [])
+
+    const oldestHistoryId = workflows
+        .map(w => parseInt(w.startHistoryId))
+        .reduce((oldest, current) => oldest < current ? oldest : current)
+        .toString()
+
+    const { history, historyId: newHistoryId } = await google.gmail("v1").users.history.list({
+        userId: "me",
+        startHistoryId: oldestHistoryId,
+        historyTypes: ["messageAdded"],
+    }).then(res => res.data)
+
+    await Promise.all(workflows.map(w => {
+        const newTrigger: any = structuredClone(w.trigger)
+        newTrigger.data.historyId = newHistoryId
+        return supabase
+            .from("workflows")
+            .update({ trigger: newTrigger })
+            .eq("id", w.id)
+            .throwOnError()
+    }))
+
+    // WILO: figuring out the best way to just set the nested fields
+    // maybe a new workflow_triggers table? not sure
 
     return NextResponse.json({
         success: "🤙",
         historyId,
         token: token?.access_token.slice(0, 10) + "...",
-        workflowIds,
+        workflows,
     })
 }
 

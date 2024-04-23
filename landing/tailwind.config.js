c1de9af209dd7269d6474de491ce6cbbe9154b09
Zach Sents <zachsents@gmail.com>
2024-04-23 07:03:25 -0600

    Added new landing page

---
+++ landing/tailwind.config.js
@@ -0,0 +1,35 @@
+const defaultColors = require("tailwindcss/colors")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./**/*.html",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: defaultColors.violet,
+            },
+        },
+    },
+    plugins: [
+        function ({ addBase, theme }) {
+            function extractColorVars(colorObj, colorGroup = '') {
+                return Object.keys(colorObj).reduce((vars, colorKey) => {
+                    const value = colorObj[colorKey]
+
+                    const newVars =
+                        typeof value === 'string'
+                            ? { [`--color${colorGroup}-${colorKey}`]: value }
+                            : extractColorVars(value, `-${colorKey}`)
+
+                    return { ...vars, ...newVars }
+                }, {})
+            }
+
+            addBase({
+                ':root': extractColorVars(theme('colors')),
+            })
+        },
+    ],
+}

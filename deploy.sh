a0896a14d3f1d6e3448dec398226f7c48c623e77
Zach Sents <zachsents@gmail.com>
2024-09-13 17:07:11 -0600

    made new deploy script and changed name of reload script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# check for required arguments
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Please provide an environment as the first argument and a service as the second argument."
+    exit 1
+fi
+
+./run.sh "$1" build "$2" && ./run.sh "$1" push "$2"
